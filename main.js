bc9731791daee71ca33687f17f122d2fb77a8ec9
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-07-26 13:04:37 +0200

    added an obstacle class, experimenting with the values

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 800;
-const CANVAS_HEIGHT = canvas.height = 700;
-let gameSpeed = 5;
+const CANVAS_WIDTH = canvas.width = 1024;
+const CANVAS_HEIGHT = canvas.height = 800;
+let gameSpeed = 1;
+const gravity = 0.5;
 //let gameFrame = 0;
 
 const backgroundLayer1 = new Image();
@@ -40,17 +41,29 @@ class Player {
     constructor() {
         this.position = {
             x: 100,
-            y: 500
+            y: 100
+        }
+        this.velocity = {
+            x: 0,
+            y: 1
         }
         this.width = 100
         this.height = 100
     }
     draw() {
+        ctx.fillStyle = `red`
         ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
-}
+    update() {
+        this.draw();
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
 
-const player = new Player();
+        if (this.position.y + this.height + this.velocity.y <= canvas.height)
+            this.velocity.y += gravity;
+        else  this.velocity.y = 0;
+    }
+}
 
 
 class Layer {
@@ -58,8 +71,7 @@ class Layer {
         this.x = 0;
         this.y = 0;
         this.width = 2400;
-        this.height = 700;
-        //this.x2 = this.width;
+        this.height = 800;
         this.image = image;
         this.speedModifier = speedModifier;
         this.speed = gameSpeed * this.speedModifier;
@@ -79,12 +91,38 @@ class Layer {
 
 const layer1 = new Layer(backgroundLayer5, 0.2);
 const layer2 = new Layer(backgroundLayer1, 0.4);
-const layer3= new Layer(backgroundLayer3, 0.6);
+const layer3 = new Layer(backgroundLayer3, 0.6);
 const layer4 = new Layer(backgroundLayer4, 0.8);
 const layer5 = new Layer(backgroundLayer2, 1);
-const layer6 = new Layer(backgroundLayer6, 0.5)
-const layer7 = new Layer(backgroundLayer7, 0.7)
-const layer8 = new Layer(backgroundLayer8,0.9)
+const layer6 = new Layer(backgroundLayer6, 0.5);
+const layer7 = new Layer(backgroundLayer7, 0.7);
+const layer8 = new Layer(backgroundLayer8,0.9);
+
+class Obstacle {
+    constructor() {
+        this.position = {
+            x: 400,
+            y: 750
+        }
+        this.width = 100
+        this.height = 50
+    }
+    draw() {
+        ctx.fillStyle = 'blue'
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height)
+    }
+}
+
+const player = new Player();
+const obstacle = new Obstacle();
+const keys = {
+    right: {
+        pressed: false
+    },
+    left: {
+        pressed: false
+    }
+}
 
 const gameObjects = [layer1, layer2, layer3, layer4, layer5, layer6, layer7, layer8]
 
@@ -93,10 +131,69 @@ function  animate() {
     gameObjects.forEach(object => {
         object.update();
         object.draw();
-        player.draw();
+        player.update()
+        obstacle.draw();
+
+        if (keys.right.pressed && player.position.x < 300) {
+            player.velocity.x = 1
+        } else if (keys.left.pressed && player.position.x > 100) {
+            player.velocity.x = -1
+        } else {
+            player.velocity.x = 0
+
+            if(keys.right.pressed) {
+                obstacle.position.x -= 1
+            } else if (keys.left.pressed) {
+                obstacle.position.x += 1
+            }
+        }
     })
     requestAnimationFrame(animate);
 };
 animate();
+
+addEventListener(`keydown`, ({keyCode}) => {
+    //console.log(keyCode);
+    switch (keyCode) {
+        case 65:
+            console.log('left')
+            keys.left.pressed = true
+            break;
+        case 83:
+            console.log('down')
+            break;
+        case 68:
+            console.log('right')
+            keys.right.pressed = true
+            break;
+        case 87:
+            console.log('up')
+            player.velocity.y -= 20;
+            break;
+    }
+    //console.log(keys.right.pressed)
+})
+
+    addEventListener(`keyup`, ({keyCode}) => {
+        //console.log(keyCode);
+        switch (keyCode) {
+            case 65:
+                console.log('left')
+                keys.left.pressed = false
+                break;
+            case 83:
+                console.log('down')
+                break;
+            case 68:
+                console.log('right')
+                keys.right.pressed = false
+                break;
+            case 87:
+                console.log('up')
+                player.velocity.y -= 0;
+                break;
+        }
+        //console.log(keys.right.pressed)
+    })
 
 })
