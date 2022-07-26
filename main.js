3e0c006c03c915dcf03aa1373256b53f9a3a8c97
Vlad Burlacu <vladburlacu1337@gmail.com>
2022-07-26 10:47:11 +0200

    added a container in the html and put the canvas inside, also added a slider to change game speed

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
-const CANVAS_WIDTH = canvas.width = 900;
-const CANVAS_HEIGHT = canvas.height = 800;
-let gameSpeed = 15;
+const CANVAS_WIDTH = canvas.width = 800;
+const CANVAS_HEIGHT = canvas.height = 700;
+let gameSpeed = 5;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = `./resources/11_background.png`;
@@ -14,6 +14,14 @@ const backgroundLayer4 = new Image();
 backgroundLayer4.src = `./resources/03_distant_trees.png`;
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = `./resources/04_bushes.png`;
+
+const slider = document.getElementById(`slider`);
+slider.value = gameSpeed;
+const showGameSpeed = document.getElementById(`showGameSpeed`);
+showGameSpeed.innerHTML = gameSpeed;
+slider.addEventListener(`change`, function (e) {
+    console.log(e.target.value);
+})
 
 class Layer {
     constructor(image, speedModifier) {
@@ -43,12 +51,20 @@ class Layer {
     }
 }
 
-const layer4 = new Layer(backgroundLayer4, 0.5);
+const layer1 = new Layer(backgroundLayer5, 0.2);
+const layer2 = new Layer(backgroundLayer1, 0.4);
+const layer3= new Layer(backgroundLayer3, 0.6);
+const layer4 = new Layer(backgroundLayer4, 0.8);
+const layer5 = new Layer(backgroundLayer2, 1);
+
+const gameObjects = [layer1, layer2, layer3, layer4, layer5]
 
 function  animate() {
     ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
-    layer4.update();
-    layer4.draw();
+    gameObjects.forEach(object => {
+        object.update();
+        object.draw();
+    })
     requestAnimationFrame(animate);
 };
 animate();
